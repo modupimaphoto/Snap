d3706b1d57f4ec4142d5c1a013d8c51d44aec75a
modupimaphoto <modupimaphoto@gmail.com>
2023-09-01 10:56:39 -0700

    added mouse listners to the features company to show when mouseenter event it triggered and to disappear when mouseleave is triggered

--- js/main.js
+++ js/main.js
@@ -34,4 +34,49 @@ close_menu.addEventListener('click', () => {
             }
         }, 20);
     };
-})
+});
+
+let timeOut;
+const features = document.querySelector(".features");
+const features_dropdown = document.querySelector(".features-dropdown");
+
+const company = document.querySelector(".company");
+const company_dropdown = document.querySelector(".company-dropdown");
+
+// Features dropdwon Event Listners.
+features.addEventListener("mouseenter", () => {
+    features_dropdown.style.display = "block";
+    company_dropdown.style.display = "none";
+    clearTimeout(timeOut);
+});
+features.addEventListener("mouseleave", () => {
+    timeOut = setTimeout(() => {
+        features_dropdown.style.display = "none";
+    }, 1200);
+});
+features_dropdown.addEventListener("mouseenter", () => {
+    features_dropdown.style.display = "block";
+    clearTimeout(timeOut);
+});
+features_dropdown.addEventListener("mouseleave", () => {
+    features_dropdown.style.display = "none";
+});
+
+// Company dropdwon Event Listners.
+company.addEventListener("mouseenter", () => {
+    company_dropdown.style.display = "block";
+    features_dropdown.style.display = "none";
+    clearTimeout(timeOut);
+});
+company.addEventListener("mouseleave", () => {
+    timeOut = setTimeout(() => {
+        company_dropdown.style.display = "none";
+    }, 1200);
+});
+company_dropdown.addEventListener("mouseenter", () => {
+    company_dropdown.style.display = "block";
+    clearTimeout(timeOut);
+});
+company_dropdown.addEventListener("mouseleave", () => {
+    company_dropdown.style.display = "none";
+});
